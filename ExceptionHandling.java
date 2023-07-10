97c7eb967868484637a2b528ca361d8b50bff331
Shwetapandey23 <139135258+Shwetapandey23@users.noreply.github.com>
2023-07-10 18:41:35 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public  class ExceptionHandling
+{
+    public static void main(String[]args)
+    {
+        int a,b,c;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Two number");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        try
+        {
+           
+           c=a/b;
+           System.out.println("Division is " +c);
+        }
+        catch(ArithmeticException e)
+        {
+        System.out.println("Denominator should not be 0,try again");
+        }
+        System.out.println("BYE");
+    }
+
+}
